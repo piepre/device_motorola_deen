b0f7f27c141b5871ee212c646a92ddd5c5df5fd0
Jyotiraditya <imjyotiraditya@pm.me>
2020-10-31 11:13:30 +0100

    deen: build libhwbinder

--- device.mk
+++ device.mk
@@ -227,6 +227,11 @@ PRODUCT_PACKAGES += \
     ims-ext-common \
     ims_ext_common.xml
 
+# HWBinder
+PRODUCT_PACKAGES += \
+    libhwbinder \
+    libhwbinder.vendor
+
 # Init
 PRODUCT_PACKAGES += \
     init.class_main.sh \
