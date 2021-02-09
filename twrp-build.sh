24bb59acd571994cde76e4490c14ceb973430167
100Daisy <100Daisy@protonmail.com>
2021-02-09 16:15:37 +0000

    prebuilts: drop prebuilt kernel

--- twrp-build.sh
+++ twrp-build.sh
@@ -11,6 +11,7 @@ repo init --depth=1 -u git://github.com/minimal-manifest-twrp/platform_manifest_
 repo sync  -f --force-sync --no-clone-bundle --no-tags -j$(nproc --all)
 # Clone device tree and common tree
 git clone --depth=1 https://github.com/100Daisy/twrp_device_motorola_deen -b android-9 device/motorola/deen
+git clone --depth=1 https://github.com/100Daisy/android_kernel_motorola_deen -b twrp kernel/motorola/msm8953
 # Build recovery image
 export ALLOW_MISSING_DEPENDENCIES=true; . build/envsetup.sh; lunch omni_deen-eng; make -j$(nproc --all) recoveryimage
 # Make the recovery installer
